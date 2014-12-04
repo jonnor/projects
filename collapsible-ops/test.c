d9a026ccde9d1558e38959eca159d8e7a3afa94d
Jon Nordby <jononor@gmail.com>
2014-12-04 02:54:36 +0100

    collapsible-ops: Found that OpenMP and/or inlining breaks autovec

--- collapsible-ops/test.c
+++ collapsible-ops/test.c
@@ -1,19 +1,58 @@
 
 // gcc -o test test.c -std=c99 -Wall -Werror `pkg-config --libs --cflags glib-2.0` -I /home/jon/contrib/code/mypaint/brushlib/tests -I /home/jon/contrib/code/mypaint/brushlib && ./test 
 
+/*
+TODO:
+* Actually have a list of (non-trivial) operations that are applied in sequence
+* Improve cache misses. Currently 15-30 % miss rate
+*/
+
 #include <glib.h>
 #include "mypaint-benchmark.c"
 
+
 static void
-pixels_mul_inplace(float *a, const float *b, int number)
+pixels_mul_inplace(const float * restrict a,
+                   const float * restrict b,
+                   float * restrict out,
+                   int number)
 {
-    for (int i=0; i<number*4; i+=4) {
-        a[i+0] = b[i+0] * a[i+0];
-        a[i+1] = b[i+1] * a[i+1];
-        a[i+2] = b[i+2] * a[i+2];
-        a[i+3] = b[i+3] * a[i+3];
+    a = __builtin_assume_aligned (a, 16);
+    b = __builtin_assume_aligned (b, 16);
+    out = __builtin_assume_aligned (out, 16);
+
+    for (int i=0; i<number*4; i+=8) {
+        #define DO_MUL(offset) \
+        { \
+            out[i+offset] = a[i+offset] * b[i+offset]; \
+        }
+        DO_MUL(0)
+        DO_MUL(1)
+        DO_MUL(2)
+        DO_MUL(3)
+        DO_MUL(4)
+        DO_MUL(5)
+        DO_MUL(6)
+        DO_MUL(7)
+        #undef DO_MUL
     }
 }
+
+/*
+static void
+pixels_mul_inplace(const float * restrict a,
+                   const float * restrict b,
+                   float * restrict out,
+                   int number)
+{
+    a = __builtin_assume_aligned (a, 16);
+    b = __builtin_assume_aligned (b, 16);
+    out = __builtin_assume_aligned (out, 16);
+    for (int i=0; i<number*4; i+=1) {
+        out[i] = a[i] * b[i];
+    }
+}
+*/
 
 /*
 static float
@@ -34,26 +73,34 @@ pixels_set(float *p, const float c[4], int n) {
 }
 
 static void
-process(float *a, float *b, int total_size, int chunk_size)
+process(const float *a, const float *b, float *out, int total_size, int chunk_size)
 {
     g_assert((total_size%chunk_size) == 0);
     const int chunks = total_size/chunk_size;
 
-    #pragma omp parallel for schedule(static)
+//    #pragma omp parallel for schedule(static)
+    // FIXME: Fix vectorization breaking when using OpenMP and not inlining
     for (int i=0; i<chunks; i++) {
         const int offset = i*chunk_size;
-        pixels_mul_inplace(&a[offset], &b[offset], chunk_size);
+        const float * restrict A = __builtin_assume_aligned(a+offset, 16);
+        const float * restrict B = __builtin_assume_aligned(b+offset, 16);
+        float * restrict O = __builtin_assume_aligned(out+offset, 16);
+        pixels_mul_inplace(A, B, O, chunk_size);
     }
 }
 
 int
 main(int argc, char *argv[])
 {
-    const int chunk_size = 64*64;
-    const int total_size = 4096*4096;
+    const int chunk_size = 128*128;
+    const int total_size = 8192*8192;
 
-    gfloat *a = g_new(gfloat, total_size*4);
-    gfloat *b = g_new(gfloat, total_size*4);
+    gfloat *a __attribute__ ((aligned (16)));
+    gfloat *b __attribute__ ((aligned (16)));
+    gfloat *out __attribute__ ((aligned (16)));
+    a = g_new(gfloat, total_size*4);
+    b = g_new(gfloat, total_size*4);
+    out = g_new(gfloat, total_size*4);
     const gfloat c1[4] = {3.0f, 3.0f, 3.0f, 3.0f};
     const gfloat c2[4] = {2.0f, 2.0f, 2.0f, 2.0f};
     pixels_set(a, c1, total_size);
@@ -61,7 +108,7 @@ main(int argc, char *argv[])
 
     // Process
     mypaint_benchmark_start("test1");
-    process(a, b, total_size, chunk_size);
+    process(a, b, out, total_size, chunk_size);
     const int process_time_ms = mypaint_benchmark_end();
 
     // Analyze
@@ -77,20 +124,20 @@ main(int argc, char *argv[])
 
     const float operation_flops_pixel = 4; // XXX: op specific
 
-
     const float max_mpixels_second = cpu_megahertz*cpu_cores*cpu_flops/operation_flops_pixel;
     const float utilization = mpixels_per_second/max_mpixels_second;
-
     g_print("Size: %.2f (mpixels) in %d chunks\n", mpixels, chunks);
-    g_print("Time: %.2f (milliseconds)\n", (double)process_time_ms);
+    g_print("Time: %.2f (milliseconds), %.2fms per chunk\n", (double)process_time_ms, (double)process_time_ms/chunks);
     g_print("Speed: %.2f (mpixels/s)\n", mpixels_per_second);
     g_print("Theoretical max: %.2f (mpixels/s)\n", max_mpixels_second);
     g_print("Utilization: %.2f %%\n", utilization*100);
 
-    g_assert(a[0] == 5.0f);
-    g_assert(a[total_size-1] == 5.0f);
+    //g_print("%f\n", out[0]);
+    g_assert(out[0] == 6.0f);
+    g_assert(out[total_size-1] == 6.0f);
 
     // Teardown
     g_free(a);
     g_free(b);
+    g_free(out);
 }
