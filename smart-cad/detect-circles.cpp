71e25aa1c2b3b670e21de4d1f24f6982005c0c07
Jon Nordby <jononor@gmail.com>
2015-04-05 19:37:06 +0200

    smart-cad: Initial FreeCAD integration of detect circles
    Can correctly insert circles as geometry in Sketch,
    when ran from Python console
    
    sys.path.append("/home/jon/projects/smart-cad")
    import smartcad
    smartcad.detect_circles(imagePlane, sketch)

--- smart-cad/detect-circles.cpp
+++ smart-cad/detect-circles.cpp
@@ -11,6 +11,41 @@
 using namespace cv;
 
 /** @function main */
+
+void
+visualize_circles(Mat src, vector<Vec3f> circles)
+{
+
+  /// Draw the circles detected
+  for( size_t i = 0; i < circles.size(); i++ )
+  {
+      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+      int radius = cvRound(circles[i][2]);
+      // circle center
+      circle( src, center, 3, Scalar(0,255,0), -1, 8, 0 );
+      // circle outline
+      circle( src, center, radius, Scalar(0,0,255), 3, 8, 0 );
+   }
+
+  /// Show your results
+  namedWindow( "Hough Circle Transform Demo", CV_WINDOW_NORMAL );
+  imshow( "Hough Circle Transform Demo", src );
+
+  while (1) {
+    waitKey(0);
+  }
+
+}
+
+void
+print_output(vector<Vec3f> circles)
+{
+    for (int i=0; i<circles.size(); i++) {
+        Vec3f &c = circles[i];
+        fprintf(stdout, "circle %f %f %f\n", c[0], c[1], c[2]); // x,y,radius
+    }
+}
+
 int main(int argc, char** argv)
 {
   Mat src, src_gray;
@@ -32,26 +67,10 @@ int main(int argc, char** argv)
   /// Apply the Hough Transform to find the circles
   HoughCircles( src_gray, circles, CV_HOUGH_GRADIENT, 1, src_gray.rows/16, 150, 30, 0, 0 );
 
-  /// Draw the circles detected
-  for( size_t i = 0; i < circles.size(); i++ )
-  {
-      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-      int radius = cvRound(circles[i][2]);
-      // circle center
-      circle( src, center, 3, Scalar(0,255,0), -1, 8, 0 );
-      // circle outline
-      circle( src, center, radius, Scalar(0,0,255), 3, 8, 0 );
-   }
-
   fprintf(stderr, "found %d circles\n", circles.size());
-
-  /// Show your results
-  namedWindow( "Hough Circle Transform Demo", CV_WINDOW_NORMAL );
-  imshow( "Hough Circle Transform Demo", src );
-
-  while (1) {
-    waitKey(0);
-  }
+  
+  print_output(circles);
+//  visualize_circles(src, circles);
 
   return 0;
 }
---
+++ smart-cad/smartcad.py
@@ -0,0 +1,33 @@
+
+import subprocess
+import FreeCAD as App
+import Part
+
+
+base = "/home/jon/projects/smart-cad"
+
+# TODO: add code for recompiling the C++ code
+
+# note, XSize and YSize are actually set by the GUI in FreeCAD?
+
+def detect_circles(image, sketch):
+
+    output = subprocess.check_output([base+'/detect-circles', image.ImageFile])
+
+    # assumes image is positioned at 0,0,0
+    topRight = App.Vector(-image.XSize/2, image.YSize/2, 0)
+
+    for line in output.split("\n"):
+        tok = line.split(' ')
+        print 'tokens', tok
+        if len(tok) >= 4 and tok[0] == 'circle':
+            x = float(tok[1])
+            y = float(tok[2])
+            radius = float(tok[3])
+            circle = Part.Circle(topRight+App.Vector(x,-y,0),App.Vector(0,0,1),radius)
+            sketch.addGeometry(circle)
+
+    return sketch
+
+if __name__ == '__main__':
+    pass
